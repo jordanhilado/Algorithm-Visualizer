7d707dcdac8e4b70a247ab05f2cdf27fab029003
tusharsharmain <imtusharsharma@gmail.com>
2020-10-12 13:48:05 +0530

    Selection Sort Added

---
+++ selectionSort.py
@@ -0,0 +1,15 @@
+import time
+
+def selection_sort(data, drawData, timeTick):
+    for i in range(len(data)-1):
+
+        min_idx = i
+        for j in range(i, len(data)):
+            if data[min_idx] > data[j]:
+                min_idx = j
+
+        data[i], data[min_idx] = data[min_idx], data[i]
+        drawData(data, ['green' if x == j or x == j+1 else 'red' for x in range(len(data))] )
+        time.sleep(timeTick)
+
+    drawData(data, ['blue' for x in range(len(data))])
--- sortingAlgs.py
+++ sortingAlgs.py
@@ -4,6 +4,7 @@ import random
 from bubbleSort import bubble_sort
 from quickSort import quick_sort
 from mergeSort import merge_sort
+from selectionSort import selection_sort
 
 root = Tk()
 root.title('Sorting Algorithm Visualizer')
@@ -60,6 +61,9 @@ def startAlgorithm():
 
     elif algMenu.get() == 'Merge Sort':
         merge_sort(data, drawData, speedScale.get())
+    
+    elif algMenu.get() == 'Selection Sort':
+        selection_sort(data, drawData, speedScale.get())
 
     drawData(data, ['green' for x in range(len(data))])
 
@@ -73,7 +77,7 @@ canvas.grid(row=1, column=0, padx=10, pady=5)
 #User Interface Area
 #Row[0]
 Label(UI_frame, text="Algorithm: ", bg='grey').grid(row=0, column=0, padx=5, pady=5, sticky=W)
-algMenu = ttk.Combobox(UI_frame, textvariable=selected_alg, values=['Bubble Sort', 'Merge Sort', 'Quick Sort'])
+algMenu = ttk.Combobox(UI_frame, textvariable=selected_alg, values=['Bubble Sort', 'Merge Sort', 'Quick Sort', 'Selection Sort'])
 algMenu.grid(row=0, column=1, padx=5, pady=5)
 algMenu.current(0)
 
