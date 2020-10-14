1739cf2af34c76c558d7aea5feb1eee9306cb553
StephenTrainor <stephontrainor@gmail.com>
2020-10-14 12:36:13 -0700

    Resolving conflicts (again)

--- sortingAlgs.py
+++ sortingAlgs.py
@@ -88,8 +88,7 @@ canvas.grid(row=1, column=0, padx=10, pady=5)
 # User Interface Area
 # Row[0]
 Label(UI_frame, text="Algorithm: ", bg='grey').grid(row=0, column=0, padx=5, pady=5, sticky=W)
-algMenu = ttk.Combobox(UI_frame, textvariable=selected_alg,
-                       values=['Bubble Sort', 'Merge Sort', 'Quick Sort', 'Insertion Sort', 'Selection Sort', 'Radix Sort'])
+algMenu = ttk.Combobox(UI_frame, textvariable=selected_alg, values=['Bubble Sort', 'Merge Sort', 'Quick Sort', 'Insertion Sort', 'Selection Sort', 'Radix Sort'])
 algMenu.grid(row=0, column=1, padx=5, pady=5)
 algMenu.current(0)
 
